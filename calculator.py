a979f93b1ce135c790b440edcfc14ae089b7919a
Aditya Rathod <adityarathod7118@gmail.com>
2026-01-26 23:16:30 +0530

    Added simple calculator program
    Basic calculator created while learning Python fundamentals.

---
+++ calculator.py
@@ -0,0 +1,21 @@
+# Simple Calculator using the fixed values
+# Created while learning python
+
+a=987
+b=123
+add=a+b
+print("The sum of a and b is:",add)
+sub=a-b
+print("the subtraction of a and b is:",sub)
+multi=a*b
+print("The multiplication of a and b is:",multi)
+divi=a/b
+print("The division of a by b is:",divi)
+
+# To find the remainder we use the operator modulus
+remainder=a%b
+print("\nThe remainder of the division is:",remainder)
+
+# Floor divisio operator is used to remove the deimal part of the answer after division
+FD=a//b
+print("\nThe division of a and b here is:",FD)
